bd376a88b4abe777770318d271740b984313debf
Eren Sakarya <53408712+erensakarya@users.noreply.github.com>
2022-07-12 23:05:21 +0300

    Update data_import_to_tables.py

--- database/data_import_to_tables.py
+++ database/data_import_to_tables.py
@@ -2,19 +2,19 @@ import sqlite3
 import pandas as pd
 from pandas import DataFrame
 
-conn = sqlite3.connect('fugo_games_case.db')
+conn = sqlite3.connect('movie.db')
 c = conn.cursor()
 
-read_tv_shows = pd.read_csv(r'***\fugo_games_case\database\tv_shows.csv')
+read_tv_shows = pd.read_csv(r'***database\tv_shows.csv')
 read_tv_shows.to_sql('TV_SHOWS', conn, if_exists='replace', index=False)  # Insert the values from the csv file into the table 'TV_SHOWS'
 
-read_cast = pd.read_csv(r'***\fugo_games_case\database\cast.csv')
+read_cast = pd.read_csv(r'***database\cast.csv')
 read_cast.to_sql('CAST', conn, if_exists='replace', index=False)  # Insert the values from the csv file into the table 'CAST'
 
-read_crew = pd.read_csv(r'***\fugo_games_case\database\crew.csv')
+read_crew = pd.read_csv(r'***database\crew.csv')
 read_crew.to_sql('CREW', conn, if_exists='replace', index=False)  # Insert the values from the csv file into the table 'CREW'
 
-read_crew = pd.read_csv(r'***\fugo_games_case\database\directors.csv')
+read_crew = pd.read_csv(r'***database\directors.csv')
 read_crew.to_sql('DIRECTORS', conn, if_exists='replace', index=False)  # Insert the values from the csv file into the table 'CREW'
 
 
